0cee33ff0a5c829d352c66d1eb79e23599ba6d06
MARCO MCNULTY <Marco@MARCOs-MacBook-Pro-2.local>
2019-10-16 16:55:22 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Blogger Routes
+  get '/bloggers/', to: 'bloggers#index', as: 'bloggers'
+  get '/bloggers/new/', to: 'bloggers#new', as: 'new_blogger'
+  post '/bloggers/', to: 'bloggers#create'
+  get '/bloggers/:id', to: 'bloggers#show', as: 'blogger'
+
+  # Posts Routes
+  get '/posts/', to: 'posts#index', as: 'posts'
+  get '/posts/new/', to: 'posts#new', as: 'new_post'
+  post '/posts/', to: 'posts#create'
+  get '/posts/:id/', to: 'posts#show', as: 'post'
+  get '/posts/:id/like/', to: 'posts#like', as: 'like'
+  get '/posts/:id/edit/', to: 'posts#edit', as: 'edit_post'
+  patch '/posts/:id', to: 'posts#update'
+
+  # Destination Routes
+  resources :destinations, only: [:show]
 end
